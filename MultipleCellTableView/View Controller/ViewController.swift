838a37226c04b21a3ce0c06e1c3f5ca07887ead7
Fabrício Masiero <masiero572@gmail.com>
2020-05-26 23:52:03 -0300

    first commit

---
+++ MultipleCellTableView/View Controller/ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  MultipleCellTableView
+//
+//  Created by Fabrício Masiero on 26/05/20.
+//  Copyright © 2020 Fabrício Masiero. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
